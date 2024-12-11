e9022d38dd38dd676c722fd739e27673e8b4121a
Georgy Litvinov <georgy.litvinov@tib.eu>
2024-12-11 07:44:24 +0100

    make endpoints private to resolve performance issues

--- harvest.sh
+++ harvest.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 #Requires jq, curl
-cd /home/gesah/LIDO
-URL_PREFIX="https://example.com"
+cd /home/litvinovg/LIDO
+URL_PREFIX="http://127.0.0.1:8080/gesah"
 VALID_OBJECTS_URL="${URL_PREFIX}/api/rest/1/cultural_object/get_valid_objects"
 SECONDS=0
 echo `date`
 echo "harvest started"
-curl -k -s -X POST $VALID_OBJECTS_URL -H  "accept: */*" -H  "Content-Type: application/json" -d "{}" -o valid_objects.json
+COOKIE=$(curl -s -k -X POST -c - ${URL_PREFIX}/api/login -H  "accept: */*" -d "username=" -d "password=")
+curl -k -s -X POST $VALID_OBJECTS_URL -H  "accept: */*" -H  "Content-Type: application/json" -d "{}" -o valid_objects.json --cookie <(echo "$COOKIE")
 VALID_OBJECTS=`cat valid_objects.json | jq -r '.cultural_object[].cultural_object.value'`
 while IFS= read -r coUri; do
     ID=`cut -d "/" -f5 <<< $coUri`
-    curl -k -s -X POST "${URL_PREFIX}/api/rest/1/cultural_object/lido_export" -H  "accept: */*" -H  "Content-Type: application/json" -d "{\"resource_id\":\"${coUri}\"}" -o response.json
+    curl -k -s -X POST "${URL_PREFIX}/api/rest/1/cultural_object/lido_export" -H  "accept: */*" -H  "Content-Type: application/json" -d "{\"resource_id\":\"${coUri}\"}" -o response.json --cookie <(echo "$COOKIE")
     IS_VALID=`cat response.json | jq -r '.valid'`
     if [ "$IS_VALID" = true ] ; then
         cat response.json | jq -r '.lido' > tmp.xml
@@ -22,11 +23,11 @@ while IFS= read -r coUri; do
             fi
             mv tmp.xml collection/${ID}.xml
         else
-            echo "Skipping xml for ${ID} as it is already the same"
+            echo "Skipping xml for ${ID} as it didn't change"
         fi
     else
         echo "response for ${ID} is invalid"
     fi
 done <<< "$VALID_OBJECTS"
 duration=$SECONDS
-echo "$((duration / 60)) minutes and $((duration % 60)) seconds elapsed.
+echo "$((duration / 60)) minutes and $((duration % 60)) seconds elapsed."
